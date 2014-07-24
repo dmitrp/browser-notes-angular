b680b2355504e65f5c4820f510ad8dc806c4f59b
Dmitrii-P <dmitrii.pisotskii@gmail.com>
2014-07-24 14:09:17 +0300

    Bring app to core code state using new structure

--- js/controllers.js
+++ js/controllers.js
@@ -4,8 +4,52 @@
 
 angular.module('myApp.controllers', [])
   .controller('ListCtrl', ['$scope', function($scope) {
+    function getStorage() {
+        var initialData = JSON.parse(localStorage.getItem('obj'));
+        if (!initialData) {
+            initialData = [];
+        }
+        return initialData;
+    };
 
+    $scope.posts = getStorage()
+
+    function syncStorage() {
+        localStorage.setItem('obj', JSON.stringify($scope.posts));
+    };
+
+    $scope.remove = function(index) {
+        $scope.posts.splice(index, 1);
+        syncStorage();
+    };
   }])
+
   .controller('AddCtrl', ['$scope', function($scope) {
+    function getCurrentDate() {
+        var d = new Date();
+        return d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
+    };
+
+    function getpostText() {
+        return document.getElementById('postText').value;
+    };
+
+    function getpostTitle() {
+        return document.getElementById('postTitle').value;
+    };
+
+    function getStorage() {
+        var initialData = JSON.parse(localStorage.getItem('obj'));
+        if (!initialData) {
+            initialData = [];
+        }
+        return initialData;
+    };
+
+    $scope.save = function() {
+        var data = getStorage();
+    	data.push({title:getpostTitle(), date:getpostText(), text:getCurrentDate()});
+        localStorage.setItem('obj', JSON.stringify(data));
+    };
 
   }]);
