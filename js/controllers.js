f057fed19a6afb97c79e0ef0fabd442eed95f238
Dmitrii-P <dmitrii.pisotskii@gmail.com>
2014-10-18 13:11:07 +0300

    Removed unnecessary code

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/* Controllers */
-
 angular.module('myApp.controllers', [])
   .controller('ListCtrl', ['$scope', function($scope) {
     var List = {
--- js/filters.js
+++ js/filters.js
@@ -1,13 +1,6 @@
 'use strict';
 
-/* Filters */
-
 angular.module('myApp.filters', [])
-  .filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    };
-  }])
   .filter('reverse', function() {
     return function(items) {
       return items.slice().reverse();
