0f8b1948e6ad7d11b45ddd1a54f7050ee3c5e4f7
dmitrp <dmitrii.pisotskii@gmail.com>
2016-01-10 10:33:22 +0200

    simplify list controller

--- js/controllers/list-ctrl.js
+++ js/controllers/list-ctrl.js
@@ -1,33 +1,28 @@
 define(['./module'], function (controllers) {
     'use strict';
-    controllers.controller('ListCtrl', ['$scope', function ($scope) {
+    controllers.controller('ListCtrl', ['$scope', 'StorageFact', function ($scope, StorageFact) {
 	    var List = {
 	        posts: [],
 	        postLimit: 3,
 	        getStorage: function() {
-	            this.posts = JSON.parse(localStorage.getItem('posts'));
-	            if (!this.posts) {
-	                this.posts = [];
-	            }
-	        },
-	        syncStorage: function() {
-	            localStorage.setItem('posts', JSON.stringify(List.posts));
+	            this.posts = StorageFact.getStorage();
 	        },
 	        removePost: function(index) {
 	            this.posts.splice(this.posts.length - index - 1, 1);
-	            this.syncStorage();
+				StorageFact.syncStorage(List.posts);
 	        },
 	        editPost: function(index) {
 	            this.posts[this.posts.length - index - 1].isEdited = true;
 	        },
 	        savePost: function(index) {
 	            this.posts[this.posts.length - index - 1].isEdited = false;
-	            localStorage.setItem('posts', JSON.stringify(this.posts));
+				StorageFact.syncStorage(List.posts);
 	        },
 	        morePosts: function() {
 	            this.postLimit += 3;
 	        }
 	    };
+
 	    List.getStorage();
 
 	    $scope.List = List;
--- js/services/storage-fact.js
+++ js/services/storage-fact.js
@@ -2,9 +2,10 @@ define(['./module'], function (services) {
     'use strict';
     services.factory('StorageFact', [function () {
         var service = {};
+        var tableName = 'posts';
 
         service.getStorage = function() {
-            var initialData = JSON.parse(localStorage.getItem('posts'));
+            var initialData = JSON.parse(localStorage.getItem(tableName));
             if (!initialData) {
                 initialData = [];
             }
@@ -14,7 +15,11 @@ define(['./module'], function (services) {
         service.saveStorage = function(record) {
             var data = this.getStorage();
             data.push(record);
-            localStorage.setItem('posts', JSON.stringify(data));
+            localStorage.setItem(tableName, JSON.stringify(data));
+        };
+
+        service.syncStorage = function(records) {
+            localStorage.setItem(tableName, JSON.stringify(records));
         };
 
         return service;
